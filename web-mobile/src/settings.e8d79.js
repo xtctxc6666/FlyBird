8ff5a200de82006537874ef46bcb504492bf8782
xtcxtc6666 <2015959927@qq.com>
2023-06-11 21:48:51 +0800

    第三次的提交

---
+++ web-mobile/src/settings.e8d79.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/BirdFly.fire",orientation:"",jsList:[],bundleVers:{internal:"d17e2",main:"3982c"}};
